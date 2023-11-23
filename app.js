79e7dcee3db49c7af58edeb08d573b8a1a30092d
James Entenia <51868095+JamesEntenia@users.noreply.github.com>
2023-11-23 23:00:52 +0800

    Update app.js
    change website_db to mysql

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ class DatabaseManager {
       host: 'james-website-db.c7vt56houjof.ap-northeast-1.rds.amazonaws.com',
       user: 'user_db',
       password: 'training',
-      database: 'website_db',
+      database: 'mysql',
     });
 
     this.connection.connect((err) => {
