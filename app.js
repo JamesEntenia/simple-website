b323e85c4ffa201eb683d7218d4a4faa2fc5b951
James Entenia <51868095+JamesEntenia@users.noreply.github.com>
2023-11-24 00:28:13 +0800

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ class DatabaseManager {
       host: 'james-website-db.c7vt56houjof.ap-northeast-1.rds.amazonaws.com',
       user: 'user_db',
       password: 'training',
-      database: 'website_db',
     });
 
     this.connection.connect((err) => {
